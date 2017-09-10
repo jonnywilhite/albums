85964d3866a1a2e21f23c92238d926c8da7b51e5
Jonathan Wilhite <jonnygwilhite@gmail.com>
2017-09-10 08:24:58 -0500

    styling and refactoring the header component

--- index.android.js
+++ index.android.js
@@ -7,10 +7,18 @@ import Header from "./src/components/header";
 
 //Create a Component
 //fat arrow functions without curly braces is a shortcut to just return the thing inside
-const App = () => (
-    //A tag with nothing inside can be made self-closing
-    <Header />
-);
+const App = () => {
+    const { headerText } = props;
+    
+    return (
+        //A tag with nothing inside can be made self-closing
+        <Header headerText={headerText} />
+    );
+};
+
+const props = {
+    headerText: "Albums"
+};
 
 //Render it to the device
 //Tells React to render an application called albums, passing in a function that returns the first component to render
